d7a2c9c07ce3ee1c4241355f99463eb2ac4abd19
James Woolfenden <james.woolfenden@gmail.com>
2021-05-10 16:27:28 +0100

    test1

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "toxic" {
 # Two suppression formats:
 # checkov:skip:
 # or
-# bridgecrew:skip=BC_AWS_NETWORKING_52:no comments
 # bridgecrew:skip=BC_AWS_S3_16: version disabled ias fine
   server_side_encryption_configuration {
     rule {
