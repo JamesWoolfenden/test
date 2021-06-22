848db41469063d8d27c1e0455ee9f34a148bbb1a
James Woolfenden <james.woolfenden@gmail.com>
2021-06-22 13:31:31 +0100

    duff code

---
+++ terraform/customer_s3.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "trustee_bucket" {
+  bucket = "redacted-bucket-name"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
