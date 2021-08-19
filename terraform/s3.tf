6059372563f673289de36d0bdf0328ca0de6369d
James Woolfenden <james.woolfenden@gmail.com>
2021-08-19 17:29:20 +0100

    who cares

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "toxic" {
 # or
 # bridgecrew:skip=BC_AWS_NETWORKING_52:no comments
 # bridgecrew:skip=BC_AWS_S3_16: version disabled ias fine
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -13,4 +14,5 @@ resource "aws_s3_bucket" "toxic" {
     }
   }
 }
+
 
