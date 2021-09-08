8250ea96c48912e29b2d3f052efc60d7977aaddc
James Woolfenden <james.woolfenden@gmail.com>
2021-09-08 10:50:00 +0100

    new code

--- terraform/azurerm/storage.tf
+++ terraform/azurerm/storage.tf
@@ -9,4 +9,5 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  enable_https_traffic_only = true
 }
