fc113fbf038823bb24e5d24997ec2f03bf03fd91
James Woolfenden <James.Woolfenden@gmail.com>
2021-10-21 19:03:42 +0100

    fudge

--- VCS/BC_GIT_6.tf
+++ VCS/BC_GIT_6.tf
@@ -4,6 +4,7 @@ resource "azuread_user" "james_woolfenden" {
   force_password_change = true
   given_name            = "James"
   job_title             = "Infrastructure Engineer"
+  #bridgecrew:skip=BC_GIT_6:We have to set a password here but changing it is enforced.
   password              = "changeme-dy2xPjgW"
   surname               = "Woolfenden"
   user_principal_name   = "james.woolfenden@gmail.com"
