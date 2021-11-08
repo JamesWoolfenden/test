e1f60ec68a12e0c20418d69527c5f6d389fb5920
James Woolfenden <james.woolfenden@gmail.com>
2021-11-08 13:21:47 +0000

    duff

--- VCS/test.tf
+++ VCS/test.tf
@@ -4,6 +4,7 @@ resource "azuread_user" "chris_densham" {
   force_password_change = true
   given_name            = "Chris"
   job_title             = "Infrastructure Engineer"
+  #bridgecrew:skip=BC_GIT_6:We have to set a password here but changing it is enforced.       <<<<<<<<<<<<<
   password            = "changeme-sy2xPjgW" # NOTE: Changing this will be forced.
   surname             = "Densham"
   user_principal_name = "chris.densham@matillion.com"
--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,9 +1,18 @@
 resource "aws_s3_bucket" "toxic" {
+	# checkov:skip=CKV_AWS_145: ADD REASON
 
 # Two suppression formats:
 # checkov:skip:
 # or
 # bridgecrew:skip=BC_AWS_NETWORKING_52:no comments
 # bridgecrew:skip=BC_AWS_S3_16: version disabled ias fine
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
+
 
