c47478d0b87c8db9b9d9407ebe394efd765e91a3
James Woolfenden <james.woolfenden@gmail.com>
2021-11-24 17:10:47 +0000

    duff

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -5,5 +5,13 @@ resource "aws_s3_bucket" "toxic" {
 # or
 # bridgecrew:skip=BC_AWS_NETWORKING_52:no comments
 # bridgecrew:skip=BC_AWS_S3_16: version disabled ias fine
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
+
 
